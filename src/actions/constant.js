e7b1892a4e29d32931aea6964a4cbbbfdad84695
huynhloc057 <huynhtanloc572001@gmail.com>
2022-11-13 13:39:38 +0700

    add back end from heroku

--- src/actions/constant.js
+++ src/actions/constant.js
@@ -109,4 +109,4 @@ export const orderConstants = {
   UPDATE_CUSTOMER_ORDER_FAILURE: "UPDATE_CUSTOMER_ORDER_FAILURE",
 };
 
-export const API_URL = "http://localhost:5000/api";
+export const API_URL = "https://apisuperfreshute.herokuapp.com/api";
